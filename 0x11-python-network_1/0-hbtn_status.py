432b5112ce035e1def9a8d7ea346ffe2f7340e4a
yusifhuseini <yusifhuseini@gmail.com>
2024-01-26 06:56:30 -0800

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 
+"""
+A python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
 import urllib.request
 
-def fetch_url(url):
-    """
-    Fetches the content of the given URL.
-
-    Args:
-    - url (str): The URL to fetch.
-
-    Returns:
-    - bytes: The content of the URL.
-    """
+def fetch_url_content(url):
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
-        content = response.read()
-    return content
+        return response.read()
 
-def print_content_info(content):
-    """
-    Prints information about the content.
-
-    Args:
-    - content (bytes): The content to print information about.
-    """
+def display_content_info(content):
     string = "Body response:\n" + f"\t- type: {type(content)}\n"
     string += f"\t- content: {content}\n"
     string += f"\t- utf8 content: {content.decode('utf-8')}"
@@ -32,8 +20,9 @@ def print_content_info(content):
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     
-    # Fetch content
-    content = fetch_url(url)
+    try:
+        content = fetch_url_content(url)
+        display_content_info(content)
+    except Exception as e:
+        print(f"Error fetching URL content: {e}")
 
-    # Print content information
-    print_content_info(content)
