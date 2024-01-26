b591015f5d7ee4724b60f848319e24dd96fcde39
yusifhuseini <yusifhuseini@gmail.com>
2024-01-26 07:07:16 -0800

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,20 +9,20 @@ import urllib.request
 def fetch_url_content(url):
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
-        return response.read()
+        content = response.read()
+    return content
 
-def display_content_info(content):
+def print_content_info(content):
     string = "Body response:\n" + f"\t- type: {type(content)}\n"
-    string += f"\t- content: {content}\n"
-    string += f"\t- utf8 content: {content.decode('utf-8')}"
+    string = string + f"\t- content: {content}\n"
+    string = string + f"\t- utf8 content: {content.decode('utf-8')}"
     print(string)
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     
-    try:
-        content = fetch_url_content(url)
-        display_content_info(content)
-    except Exception as e:
-        print(f"Error fetching URL content: {e}")
-
+    # Fetch URL content
+    page_content = fetch_url_content(url)
+    
+    # Print information about the content
+    print_content_info(page_content)
