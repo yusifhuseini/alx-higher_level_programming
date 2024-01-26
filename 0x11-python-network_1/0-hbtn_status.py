66d26d210ae1c101754787977ff13aefb5f3816d
yusifhuseini <yusifhuseini@gmail.com>
2024-01-26 06:51:40 -0800

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
 
-"""
-A python script that fetches https://alx-intranet.hbtn.io/status
-"""
-
 import urllib.request
 
-if __name__ == '__main__':
-    url = 'https://alx-intranet.hbtn.io/status'
+def fetch_url(url):
+    """
+    Fetches the content of the given URL.
+
+    Args:
+    - url (str): The URL to fetch.
+
+    Returns:
+    - bytes: The content of the URL.
+    """
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         content = response.read()
-        string = "Body response:\n" + f"\t- type: {type(content)}\n"
-        string = string + f"\t- content: {content}\n"
-        string = string + f"\t- utf8 content: {content.decode('utf-8')}"
-        print(string)
+    return content
+
+def print_content_info(content):
+    """
+    Prints information about the content.
+
+    Args:
+    - content (bytes): The content to print information about.
+    """
+    string = "Body response:\n" + f"\t- type: {type(content)}\n"
+    string += f"\t- content: {content}\n"
+    string += f"\t- utf8 content: {content.decode('utf-8')}"
+    print(string)
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    # Fetch content
+    content = fetch_url(url)
+
+    # Print content information
+    print_content_info(content)
