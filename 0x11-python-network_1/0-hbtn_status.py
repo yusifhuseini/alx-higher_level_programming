75d1ff70f4db41582cf3fbac14d7f3f613afc0cc
yusifhuseini <yusifhuseini@gmail.com>
2024-01-26 05:47:19 -0800

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
+
 """
-Fetches https://alx-intranet.hbtn.io/status using urllib
+A python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 import urllib.request
+from urllib.error import URLError, HTTPError
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    utf8_content = content.decode('utf-8')
-
-print("Body response:")
-print("\t- type:", type(content))
-print("\t- content:", content)
-print("\t- utf8 content:", utf8_content)
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            content = response.read()
+            string = "Body response:\n" + f"\t- type: {type(content)}\n"
+            string += f"\t- content: {content}\n"
+            string += f"\t- utf8 content: {content.decode('utf-8')}"
+            print(string)
+    except HTTPError as e:
+        print(f"HTTPError: {e.code}")
+    except URLError as e:
+        print(f"URLError: {e.reason}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
