287982c3092ed6cd06729e218e76c4ae0f4ad0a8
yusifhuseini <yusifhuseini@gmail.com>
2024-01-26 07:20:05 -0800

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,19 +6,12 @@ A python script that fetches https://alx-intranet.hbtn.io/status
 
 import urllib.request
 
-def fetch_url_content(url):
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         content = response.read()
-    return content
-
-def print_content_info(content):
-    string = "Body response:\n" + f"\t- type: {type(content)}\n"
-    string = string + f"\t- content: {content}\n"
-    string = string + f"\t- utf8 content: {content.decode('utf-8')}"
-    print(string)
-
-if __name__ == '__main__':
-    url = 'https://alx-intranet.hbtn.io/status'
-    page_content = fetch_url_content(url) # Fetch URL content
-    print_content_info(page_content) # Print info about the content
+        string = "Body response:\n" + f"\t- type: {type(content)}\n"
+        string = string + f"\t- content: {content}\n"
+        string = string + f"\t- utf8 content: {content.decode('utf-8')}"
+        print(string)
